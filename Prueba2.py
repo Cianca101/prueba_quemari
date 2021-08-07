6d62be6b4829bf9bb94556c3a7e89b187d965dc4
Cianca101 <emmanuel.carrillo.c@gmail.com>
2021-08-06 21:37:47 -0500

    ADD Pruebas1-2

---
+++ Prueba2.py
@@ -0,0 +1,7 @@
+# 2. Genera un script que reciba una lista y ordene sus elementos de menor a mayor.
+
+def script(input):
+    lista = input.split(",")
+    lista.sort()
+    return lista
+
---
+++ Prueba3.py
@@ -0,0 +1,5 @@
+# 3. Genera un script que muestre en pantalla el nombre de todos los estudiantes
+# cuya nota de examen es superior a la media y hayan entregado la práctica. (traer
+# datos de un archivo)
+
+
